75ab54c2c7bf65014a6edda9f9a5805123895380
Weslley do Cati Ferreira <weslley.docati@gmail.com>
2021-09-01 01:56:00 -0300

    Implemented getOne and contries methods in the Country resource class

--- src/main/java/com/github/weslleycf/citiesapi/countries/resources/CountryResource.java
+++ src/main/java/com/github/weslleycf/citiesapi/countries/resources/CountryResource.java
@@ -4,11 +4,15 @@ package com.github.weslleycf.citiesapi.countries.resources;
 
 import com.github.weslleycf.citiesapi.countries.entities.Country;
 import com.github.weslleycf.citiesapi.countries.repositories.CountryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -21,8 +25,19 @@ public class CountryResource {
     }
 
     @GetMapping
-    public List<Country> countries(){
-        return repository.findAll();
+    public Page<Country> countries(Pageable page){
+        return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Country> getOne(@PathVariable Long id){
+        Optional<Country> optional = repository.findById(id);
+
+        if (optional.isPresent()){
+            return ResponseEntity.ok().body(optional.get());
+        }   else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 
